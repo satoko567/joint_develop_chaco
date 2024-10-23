688af2c713a2df4de5f612c076d860b12a85a518
shintaro yasuda <shin38@ymail.ne.jp>
2024-10-23 20:24:15 +0900

    最終チェック

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="mt-5">
     <nav class="navbar navbar-dark bg-info justify-content-center">
-        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
+        <span class="navbar-brand">Shintaro Yasuda and staffs of the willing, All rights reserved.</span>
     </nav>
 </footer>
