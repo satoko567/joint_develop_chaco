7951755ff233c825599eeed695e02ef2a9e3b233
Shinya-Nakaguchi <snfjkdh@gmail.com>
2024-10-27 23:11:17 +0900

    #115 ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 Route::get('/', 'PostsController@index');
 
 // ユーザ
-Route::get('/', 'UsersController@index');
 Route::prefix('users')->group(function () {
     Route::get('{id}', 'UsersController@show')->name('user.show');
 });
