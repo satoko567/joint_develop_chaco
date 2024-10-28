fed0bf097cb174decd0f467c9b9e41e8a2863c3f
izumi sakamoto <zmskmt1999@gmail.com>
2024-10-28 22:20:50 +0900

    #117 ユーザ編集画面・更新(いずみ)

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\User;
+
+class UsersController extends Controller
+{
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $user = User::findOrFail($id);
+        $data = ['user' => $user,];
+
+        return view('user.edit', $data);
+    }
+    
+    public function update(UserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->
+        $user->save();
+        
+        return back
+    }
+}
