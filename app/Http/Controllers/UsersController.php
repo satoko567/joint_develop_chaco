5f231b6bca4298f98e8b31dc7a423a0977ae98bb
izumi sakamoto <zmskmt1999@gmail.com>
2024-10-29 13:32:34 +0900

    #117 ユーザ編集画面・更新(いずみ)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends Controller
         $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->
+        $user->password = $request->Hash::make($request->password);
         $user->save();
         
         return back
