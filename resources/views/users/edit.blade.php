6be945c09786bcf116fca0f229a28c16f731bec8
izumi sakamoto <zmskmt1999@gmail.com>
2024-11-02 13:00:01 +0900

    #117 ユーザ編集画面・更新(いずみ)

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,6 +2,8 @@
 @section('content')
 <h2 class="mt-5 mb-3">ユーザ情報を編集する</h2>
     <form method="POST" action="{{ route('user.update', $user->id) }}">
+        {{-- Error Messages --}}
+        @include('commons.error_messages')
         @csrf
         @method('PUT')
         <input type="hidden" name="id" value="{{ old('id', $user->id) }}" />
