f5ddecee681e7ada4af71c73cc331f928a761767
yumi hada <setsuna-1119@outlook.com>
2024-11-09 11:48:12 +0900

    #121 投稿削除

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('/', 'PostsController@index');
+
+
+Route::delete('posts/{id}', 'PostsController@destroy')->name('movie.delete'); //ユーザ削除
