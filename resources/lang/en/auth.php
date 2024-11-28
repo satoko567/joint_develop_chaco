5096074d57871a3ae2c35ba43fcc4e2c7ba43d26
yasushi sato <xyz.20131984@gmail.com>
2024-11-29 08:24:47 +0900

    #111 バリデーションメッセージの日本語化

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '認証に失敗しました。',
+    'throttle' => 'ログイン試行が規定回数を超えました。:seconds秒後に再開できます。',
+    // 'failed' => 'These credentials do not match our records.',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
