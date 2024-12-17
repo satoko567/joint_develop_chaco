7f2701a54ac9eaf1de07fdf50b09a99f95f86267
izumi sakamoto <zmskmt1999@gmail.com>
2024-12-18 02:31:20 +0900

    検索結果とコメント覧のユーザプロフィール画像の表示とページネーション

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -7,7 +7,11 @@
             @foreach ($users as $user)
                 <li class="mb-3 text-center">
                     <div class="text-left d-inline-block w-75 mb-2">
-                        <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="ユーザのアバター画像">
+                        @if($user->avatar)
+                            <img class="mr-2 rounded-circle" src="{{ Storage::url($user->avatar) }}" alt="プロフィール画像" style="width: 55px; height: 55px;">
+                        @else
+                            <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email, 55) }}" alt="アバター画像">
+                        @endif
                         <p class="mt-3 mb-0 d-inline-block">
                             <a href="{{ route('user.show', $user->id) }}" class="mr-3">{{ $user->name }}</a>
                         </p>
@@ -18,9 +22,9 @@
                                 <p class="mb-0">{{ $post->content }}</p>
                                 <p class="text-muted">{{ $post->created_at }}</p>
                                 <div class="d-flex align-items-center">
-                                    <a href="{{ route('post.show', $post->id) }}" class="mr-2">
-                                        <i class="fas fa-comment"></i> <!-- 吹き出しアイコン -->
-                                        {{ $post->comments_count ?? 0 }}
+                                    <a href="{{ route('post.show', $post->id) }}" class="text-decoration-none mr-2">
+                                        <i class="fas fa-comment text-muted"></i> <!-- 吹き出しアイコン -->
+                                        <span class="text-muted">{{ $post->comments_count ?? 0 }}</span>
                                     </a>
                                     @include('commons.like_button') <!--Like Button -->
                                     @include('commons.bookmark_button', ['post' => $post]) <!-- Bookmark Button -->
