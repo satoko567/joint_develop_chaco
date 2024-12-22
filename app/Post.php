ff7534b9aae4412aa8a6f9cabbf03663afd55220
yumi hada <setsuna-1119@outlook.com>
2024-12-22 10:41:25 +0900

    いいね！機能

--- app/Post.php
+++ app/Post.php
@@ -18,4 +18,9 @@ class Post extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function favoriteUsers()
+    {
+        return $this->belongsToMany(User::class,'favorites','post_id','user_id')->withTimestamps();
+    }
 }
