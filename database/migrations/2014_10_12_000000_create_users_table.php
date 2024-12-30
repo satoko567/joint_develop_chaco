0e93688176fe403d9e1fa4cad83635931a457b6f
jin 6hara <job6hara@gmail.com>
2024-12-30 11:21:48 +0900

    変更ないはずですが、プランチ切り替えの際エラー出たたので念のため

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,15 +17,14 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('email')->unique();
             $table->string('nickname')->unique();
-            $table->string('password');            
-            $table->enum('gender', ['male', 'female', 'other','unknown'])->default('unknown');
-            $table->string('profile_picture')->nullable(); // デフォルト写真を管理
-            $table->text('self_introduction')->nullable(); // 自己紹介用カラムを追加
+            $table->string('password');
+            $table->enum('gender', ['male', 'female', 'other', 'unknown'])->default('unknown');
+            $table->string('profile_picture')->nullable();
+            $table->text('self_introduction')->nullable();
             $table->timestamps();
             $table->softDeletes();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            
         });
     }
 
