6d71e8549d0ad6cbda6b875c8b1e42ff2b62edd4
miyagi <yagi3885@gmail.com>
2025-01-09 10:45:39 +0900

    #95 posts（投稿）テーブルのマイグレーションとシーダー作成

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    // 投稿情報からユーザ情報を取得
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * 12件の投稿テストデータ作成
+     * 
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i = 1; $i <= 12; $i++) {
+            DB::table('posts')->insert([
+                'user_id' => (($i - 1) % 4) + 1, // 1から4までのuser_idを繰り返し
+                'content' =>  $i . '番目のテスト投稿です！',
+                'created_at' => now(),
+            ]);
+        }
+    }
+}
