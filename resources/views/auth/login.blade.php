c0b9086152a55487a59040409e4329f8c6c0e073
takuma arakawa <itoj.arakawa@gmail.com>
2025-01-19 00:23:26 +0900

    #111 バリデーションメッセージの日本語化

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,13 @@
     </div>
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
+            @if (count($errors) > 0)
+                <ul class="alert alert-danger" role="alert">
+                    @foreach ($errors->all() as $error)
+                        <li class="ml-4">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
             <form method="POST" action="{{ route('login.post') }}">
                 @csrf
                 <div class="form-group">
