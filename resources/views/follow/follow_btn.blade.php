173fd178a48726a88792fd5fd35861e34bc3b051
yuusuke ito <yuu.is3mt@gmail.com>
2025-02-08 01:44:09 +0900

    #127 フォロー機能　修正

--- resources/views/follow/follow_btn.blade.php
+++ resources/views/follow/follow_btn.blade.php
@@ -1,13 +1,15 @@
-@if (Auth::id() !== $user->id) 
-    @if (Auth::user()->followings->contains($user->id))
-        <form method="POST" action="{{ route('unfollow', $user->id) }}">
-            @csrf
-            <button type="submit" class="btn btn-danger">フォロー解除</button>
-        </form>
-    @else
-        <form method="POST" action="{{ route('follow', $user->id) }}">
-            @csrf
-            <button type="submit" class="btn btn-primary">フォロー</button>
-        </form>
+@if (Auth::check())
+    @if (Auth::id() !== $user->id) 
+        @if (Auth::user()->followings->contains($user->id))
+            <form method="POST" action="{{ route('unfollow', $user->id) }}">
+                @csrf
+                <button type="submit" class="btn btn-danger">フォロー解除</button>
+            </form>
+        @else
+            <form method="POST" action="{{ route('follow', $user->id) }}">
+                @csrf
+                <button type="submit" class="btn btn-primary">フォロー</button>
+            </form>
+        @endif
     @endif
 @endif
