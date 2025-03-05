ea45de94fbb3e072dd979c651dd70c0373b7ddab
setono422 <saltriver0625@gmail.com>
2025-03-06 05:25:59 +0900

    postsテーブルのシーダー修正（エラー修正）

--- database/migrations/2025_03_03_082629_create_posts_table.php
+++ database/migrations/2025_03_03_082629_create_posts_table.php
@@ -16,17 +16,14 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) 
         {
             $table->bigIncrements('id'); // 投稿ID（自動採番）
-            $table->unsignedBigInteger('user_id'); // ユーザーID（外部キー）
-            $table->string('title'); // 投稿タイトル
-            $table->text('content'); // 投稿内容
+            $table->unsignedBigInteger('user_id')->index(); // ユーザーID（外部キー + 検索高速化）
+            $table->string('content', 140); // 投稿内容（最大140文字）
             $table->timestamps(); // created_at, updated_at
             $table->softDeletes(); // ソフトデリート（論理削除）
 
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
-            // 検索高速化
-            $table->index('user_id');
         });
     }
 
