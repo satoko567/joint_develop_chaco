01de8b829f4c4faefb01c22b94e5bd2913f8276e
KzAshimo <good.ashimo@gmail.com>
2025-03-08 22:36:44 +0900

    controller途中

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::prefix('/signup')->group(function () {
     Route::get('/', 'Auth\RegisterController@showRegistrationForm')->name('signup');
     Route::post('/','Auth\RegisterController@register')->name('signup.post');
 });
+
+//user詳細
+Route::prefix('/users')->group(function(){
+    Route::get('/{id}','UsersDetailController@show')->name('user.show');
+});
