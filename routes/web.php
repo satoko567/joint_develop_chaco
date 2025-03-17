5763e7931d4ec897ec8986500d66298246387a5f
KzAshimo <good.ashimo@gmail.com>
2025-03-17 22:05:54 +0900

    route修正

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostsController@index');
 // 投稿処理
-Route::prefix('/posts')->middleware('auth')->group(function(){
-    Route::post('/','PostsController@store')->name('post.store'); // 新規投稿処理
+Route::group(['middleware' => 'auth'], function(){
+    // 以下、ログイン後のみ実行できるルーティングを記述可能
+    Route::prefix('/posts')->group(function(){
+        Route::post('/','PostsController@store')->name('post.store'); // 新規登録処理
+        // 以下、その他post関連のルーティングを記述可能
+    });
 });
 
 // user新規登録処理
 Route::prefix('/signup')->group(function () {
-    Route::get('/', 'Auth\RegisterController@showRegistrationForm')->name('signup'); //画面表示
-    Route::post('/','Auth\RegisterController@register')->name('signup.post'); //登録処理
+    Route::get('/', 'Auth\RegisterController@showRegistrationForm')->name('signup'); // 画面表示
+    Route::post('/','Auth\RegisterController@register')->name('signup.post'); // 登録処理
 });
 
 // user詳細
