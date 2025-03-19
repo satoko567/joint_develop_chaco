b003d746ab52bfbbac46e92085e780eb59f2ca45
kosuke.chimata <chimatakousuke@gmail.com>
2025-03-20 07:29:51 +0900

    新規投稿画面の表示

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 //ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+//ログイン後
+    //新規投稿
+//    Route::group(['middleware' => 'auth'], function () {
+        Route::get('post', 'PostingController@showPostingForm')->name('post');
+        Route::post('post', 'PostingController@store')->name('post.store');
+//    });
