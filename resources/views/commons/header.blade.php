cb9bc1f768cf0b5d5f478bfb66dc0faaf94e95bb
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-01 02:17:55 +0900

    topをindexに変更

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -8,10 +8,10 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 @if (Auth::check())
-                    <li class="nav-item"><a href="{{ route('top') }}" class="nav-link">ログアウト</a></li>  {{-- routeのアドレス'top'はダミーで書いた。後にログインルートネームに変更する必要がある。Rikoさんへ連絡。 --}}
+                    <li class="nav-item"><a href="{{ route('index') }}" class="nav-link">ログアウト</a></li>  {{-- routeのアドレス'top'はダミーで書いた。後にログインルートネームに変更する必要がある。Rikoさんへ連絡。 --}}
                     <li class="nav-item"><a href="{{ route('users.show' , Auth::id()) }}" class = "nav-link"><span class="user-name">{{ Auth::user()->name }}</span></a></li>
                 @else
-                    <li class="nav-item"><a href="{{ route('top') }}" class="nav-link">ログイン</a></li>  {{-- routeのアドレス'top'はダミーで書いた。後にログインルートネームに変更する必要がある。Rikoさんへ連絡。 --}}
+                    <li class="nav-item"><a href="{{ route('index') }}" class="nav-link">ログイン</a></li>  {{-- routeのアドレス'top'はダミーで書いた。後にログインルートネームに変更する必要がある。Rikoさんへ連絡。 --}}
                     <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
                 @endif
             </ul>
