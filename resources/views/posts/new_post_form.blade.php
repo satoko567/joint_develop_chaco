105cc833bd61a3ec11b664f26e4d8d6f8e1a7084
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-03 07:10:35 +0900

    ちょっと訂正

--- resources/views/posts/new_post_form.blade.php
+++ resources/views/posts/new_post_form.blade.php
@@ -17,7 +17,7 @@
         <form method="POST" action="{{route('post.store')}}">
             @csrf
             <div class="form-group">
-                <textarea type="content" class="form-control custom-textarea" name="content" rows="4" value="{{old('content')}}"></textarea>
+                <textarea type="content" class="form-control custom-textarea" name="content" rows="4"></textarea>
                 <div class="text-left mt-3">
                     <button type="submit" class="btn btn-primary mt-2">投稿する</button>
                 </div>
