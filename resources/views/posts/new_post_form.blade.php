6f1e3d0fcb5b14e3aae54bea1ff99e275954e85b
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-03 06:58:48 +0900

    新規投稿機能　動作確認完了

--- resources/views/posts/new_post_form.blade.php
+++ resources/views/posts/new_post_form.blade.php
@@ -1,21 +1,27 @@
 @extends('layouts.app')
 @section('content')
-<div class="text-center">
-    <h1><i class="fa-solid fa-face-grin-wide"></i>Topic Post</h1>
+<div class="center jumbotron bg-info">
+    <div class="text-center text-white mt-2 pt-1">
+        <h1>
+            <i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts
+        </h1>
+    </div>
 </div>
+<h5 class="description text-center">"○○"について140文字以内で会話しよう！</h5>
 
-<div class="text-center mt-3">
-    <p class="text-left d-inline-block">"〇〇"について140文字以内で会話しよう！</p>
-</div>
-
+{{-- バリデーションエラーの表示 --}}
 @include('commons.error_messages')
-<div class="text-center mb-3">
+
+{{-- 投稿フォーム --}}
+<div class="text-center mt-3 mb-3">
         <form method="POST" action="{{route('post.store')}}">
             @csrf
             <div class="form-group">
-                <textarea type="content" class="form-control" name="content" rows="4" value="{{old('content')}}"></textarea>
+                <textarea type="content" class="form-control custom-textarea" name="content" rows="4" value="{{old('content')}}"></textarea>
+                <div class="text-left mt-3">
+                    <button type="submit" class="btn btn-primary mt-2">投稿する</button>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary mt-2">投稿する</button>
         </form>
 </div>
 @endsection
