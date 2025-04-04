7a060c92881c1d1138079506863b81dcfaea3ff0
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-05 07:32:20 +0900

    新規投稿機能　ご確認3回目後　訂正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,6 @@
     {{-- ログインしていると、投稿フォームが表示される。してないと表示されない。 --}}
     @if (Auth::check())
         @include('posts.new_post_form')
-    @else 
     @endif
 
 @endsection
--- resources/views/posts/new_post_form.blade.php
+++ resources/views/posts/new_post_form.blade.php
@@ -1,14 +1,3 @@
-@extends('layouts.app')
-@section('content')
-<div class="center jumbotron bg-info">
-    <div class="text-center text-white mt-2 pt-1">
-        <h1>
-            <i class="fab fa-telegram fa-lg pr-3"></i>Topic Posts
-        </h1>
-    </div>
-</div>
-<h5 class="description text-center">"○○"について140文字以内で会話しよう！</h5>
-
 {{-- バリデーションエラーの表示 --}}
 @include('commons.error_messages')
 
@@ -24,4 +13,3 @@
             </div>
         </form>
 </div>
-@endsection
