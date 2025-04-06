67859293c10418d2ec378e656e56f273151ee9c7
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-07 06:20:15 +0900

    ルートをちょっと修正

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', 'PostsController@index')->name('index');
 //ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 //ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
