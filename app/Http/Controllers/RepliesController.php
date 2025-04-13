69338feedb0999096864e5de1c875dfa242afe13
setono422 <saltriver0625@gmail.com>
2025-04-13 13:36:18 +0900

    リプライ投稿機能の追加：store処理、Bladeフォーム、トップページ戻る導線

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Reply;
+use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
@@ -10,5 +12,23 @@ class RepliesController extends Controller
     {
         $replies = $post->replies()->with('user')->latest()->paginate(10);
         return view('replies.replies_for_post', compact('post', 'replies'));
+    }
+
+    public function store(Request $request, Post $post)
+    {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|string|max:255',
+        ]);
+
+        // リプライ作成
+        Reply::create([
+            'post_id' => $post->id,
+            'user_id' => auth()->id(),
+            'content' => $request->input('content'),
+        ]);
+
+        // 元のページにリダイレクト
+        return redirect()->route('replies.index', $post->id);
     }
 }
