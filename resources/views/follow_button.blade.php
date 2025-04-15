0ebb96b208e6cca5ca53108ce0306fdbe6f89190
kosuke.chimata <chimatakousuke@gmail.com>
2025-04-16 02:45:31 +0900

    フォロー機能　いいね数の表示

--- resources/views/follow_button.blade.php
+++ resources/views/follow_button.blade.php
@@ -1,23 +1,41 @@
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-</head>
-@php
-    $exists = Auth::user()->isFollow($post->id);
-@endphp
+@if (Auth::id()!== $post->user_id)
+    @php
+        $exists = Auth::check() ? Auth::user()->isFollow($post->id) : false;
+        $countFollowUsers = $post->followUsers()->count();
+    @endphp
 
-@if ($exists)
-    <form method="POST" action="{{ route('follow.delete', $post->id) }}">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-link p-0 border-0 bg-transparent">
-            <i class="fa-regular fa-thumbs-up mb-3"></i>
-        </button>
-    </form>
-@else
-    <form method="POST" action="{{ route('follow.store', $post->id) }}">
-        @csrf
-        <button type="submit" class="btn btn-link p-0 border-0 bg-transparent">
-            <i class="fa-solid fa-thumbs-up mb-3"></i>
-        </button>
-    </form>
+    @if ($exists)
+        <form method="POST" action="{{ route('follow.delete', $post->id) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-link p-0 border-0 bg-transparent">
+                <i class="fa-regular fa-thumbs-up mb-3 mr-1"></i>{{$countFollowUsers}}
+            </button>
+        </form>
+    @else
+        <form method="POST" action="{{ route('follow.store', $post->id) }}">
+            @csrf
+            <button type="submit" class="btn btn-link p-0 border-0 bg-transparent">
+                <i class="fa-solid fa-thumbs-up mb-3 mr-1"></i>{{$countFollowUsers}}
+            </button>
+        </form>
+    @endif
+
+    <script>
+        // スクロール位置を記録
+        document.querySelectorAll('form').forEach(form => {
+            form.addEventListener('submit', function () {
+                sessionStorage.setItem('scrollPosition', window.scrollY);
+            });
+        });
+    
+        // ページロード後に元の位置へスクロール
+        window.addEventListener('load', function () {
+            const scrollY = sessionStorage.getItem('scrollPosition');
+            if (scrollY !== null) {
+                window.scrollTo(0, parseInt(scrollY));
+                sessionStorage.removeItem('scrollPosition'); // 1回だけ使う
+            }
+        });
+    </script>
 @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> {{-- 様々なファイルの表示を、追加訂正するためのCSS --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     </head>
     <body>
         @include('commons.header')
