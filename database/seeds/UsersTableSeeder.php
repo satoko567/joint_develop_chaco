07e96ef2094bee0f68f837085c8edd5674b36ca9
tatsunori akasaka <tatsunori@example.com>
2025-05-05 06:58:25 +0000

    インデント修正・usersテーブル

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,27 +15,27 @@ class UsersTableSeeder extends Seeder
     {
         DB::table ('users')->insert([
             [
-             'name' => 'test1',
-             'email' => 'test1@test.com',
-             'password' => Hash::make ('test1')
+                'name' => 'test1',
+                'email' => 'test1@test.com',
+                'password' => Hash::make ('test1')
             ],
 
             [
-             'name' => 'test2',
-             'email' => 'test2@test.com',
-             'password' => Hash::make ('test2')
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => Hash::make ('test2')
             ],
 
             [
-             'name' => 'test3',
-             'email' => 'test3@test.com',
-             'password' => Hash::make ('test3')
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'password' => Hash::make ('test3')
             ],
 
             [
-             'name' => 'test4',
-             'email' => 'test4@test.com',
-             'password' => Hash::make ('test4')
+                'name' => 'test4',
+                'email' => 'test4@test.com',
+                'password' => Hash::make ('test4')
             ]
         ]);
     }
