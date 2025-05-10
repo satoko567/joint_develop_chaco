4f7042f772402095642100901ee12fbde944bb1c
tatsunori-1550 <aba31209875cc@gmail.com>
2025-05-10 21:17:57 +0900

    WIP トップページの投稿表示作業途中も保存

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        
+        $posts = Post::orderBy ('id','desc')->paginate (10);
+        
+        return view ('welcome', [
+            'posts' => $posts,
+        ]);
+    }
+}
