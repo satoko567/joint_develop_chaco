7e12b9b1f1bd585df5fd57074ba39be88e8286db
satoko <kwutian253@gmail.com>
2025-05-14 16:46:40 +0900

    #117 ユーザ編集画面・更新

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,16 @@ Route::get('/', function () {
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-
 // ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+   // ユーザ編集、更新、退会
+   Route::prefix('users/{id}')->group(function () {
+       Route::get('edit', 'UsersController@edit')->name('user.edit');
+       Route::put('', 'UsersController@update')->name('user.update');
+   });
+});
