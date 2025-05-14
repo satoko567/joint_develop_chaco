bdaa53d60bf2de88dbe5d33b0ae3b79eed010d66
mitsunobu.morio <m.morio@rhythm.ocn.ne.jp>
2025-05-14 02:01:09 +0000

    #115 user details

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,10 @@
         <div class="text-center mb-3">
             <form method="" action="" class="d-inline-block w-75">           
                 <div class="form-group">
-                 <textarea class="form-control" name="" rows=""></textarea>
-                    <div class="text-left mt-3">
-                  <button type="submit" class="btn btn-primary">投稿する</button>
-                 </div>
+                    <textarea class="form-control" name="" rows=""></textarea>
+                        <div class="text-left mt-3">
+                            <button type="submit" class="btn btn-primary">投稿する</button>
+                        </div>
                 </div>
             </form>
         </div>
