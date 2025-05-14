a8a9ec11d9cf8761a40e3d78eb46cecc36409c90
mitsunobu.morio <m.morio@rhythm.ocn.ne.jp>
2025-05-14 10:08:10 +0000

    #115 user details

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,8 +6,8 @@
                 <div class="card-header">
                     <h3 class="card-title text-light">{{$user->name}}</h3>
                 </div>
-                <div class="card-body text-center">
-                    <img class="mr-2 rounded-circle" src="{{ Gravatar::src($user->email , 310) }}" alt="ユーザのアバター画像">
+                <div class="card-body">
+                    <img class="rounded-circle" src="{{ Gravatar::src($user->email , 310) }}" alt="ユーザのアバター画像">
                         <div class="mt-3">
                             <a href="{{ '' }}" class="btn btn-primary btn-block">ユーザ情報の編集</a>
                         </div>
