2de8d8c0e82fb686b1b39c955dc172ebfc5719d6
tomomi yashiki <wagona3667@gmail.com>
2025-06-06 02:38:30 +0900

    投稿の検索機能

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,28 @@
             <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>寺子屋＠プログラミング</h1>
         </div>
     </div>
+    <form method="GET" action="{{ route('post.index') }}" class="mx-auto mt-4" style="width: 500px;">
+     <div class="form-group ">
+         <div class="input-group">
+             <input type="text" name="keyword" class="form-control" placeholder="検索"  value="{{ old('keyword', $keyword) }}" autocomplete="on">
+             <div class="input-group-append">
+                 <button type="submit" class="btn" style="background-color: #17A2B8; color: white;">検索</button>
+             </div>
+         </div>
+      </div>
+    </form>
+    <ul>
+        @if (!empty($keyword) && $posts->isEmpty())
+            <p class="mt-4 mb-3 text-center">「{{ $keyword }}」に一致する投稿はありませんでした。</p>
+        @elseif ($posts->isNotEmpty())
+            @foreach ($posts as $post)
+                <!-- 投稿の表示 -->
+            @endforeach
+        @else
+            <p>投稿がまだありません。</p>
+        @endif
+    </ul>
+    {{ $posts->links() }}
     <h5 class="description text-center mb-3">"○○"について140字以内で会話しよう！</h5>
     @if (Auth::check())
         <div class="w-75 m-auto">@include('commons.error_messages')</div> 
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/', 'PostsController@index')->name('post.index');
