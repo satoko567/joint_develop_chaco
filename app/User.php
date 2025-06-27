016bc2c1bc7f09fb6dc62354a74dd4945613f18b
Miko <mikodesu0415@gmail.com>
2025-06-28 00:00:04 +0900

    push nice

--- app/User.php
+++ app/User.php
@@ -1,10 +1,12 @@
 <?php
 namespace App;
+
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Gravatar;
 
 class User extends Authenticatable
 {
