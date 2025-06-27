8ad126f8246a7008ebde6d59f7293e922689cbea
mitsunobu.morio <m.morio@rhythm.ocn.ne.jp>
2025-06-27 16:12:07 +0000

    Add autocomplete attribute to email input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
             @include('components.flash_message')
             <div class="form-group">
                 <label for="email">メールアドレス</label>
-                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
+                <input id="email" type="email" name="email" class="form-control" value="{{ old('email') }}" autocomplete="email" required>
             </div>
             <div class="form-group">
                 <label for="password">パスワード</label>
